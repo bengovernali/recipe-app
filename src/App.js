5af733c54efc3b4f9000f4c16d0289d91e9800d0
Ben Governali <bgovern93@gmail.com>
2020-04-01 16:14:50 -0400

    changed navbar color. added initial tiles for home screen

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
       <div className="Nav">
         <h1 className="title">Recipe App</h1>
       </div>
+      <div className="tile-grid">
+        <div className="tile">
+          <h3>Random Recipe</h3>
+        </div>
+        <div className="tile">
+          <h3>Random by Cuisine</h3>
+        </div>
+        <div className="tile">
+          <h3>Cuisines</h3>
+        </div>
+      </div>
     </div>
   );
 }
