b5a5dcea7475334ee692e2b203c14e3459e7ea8f
Ben Governali <bgovern93@gmail.com>
2020-06-04 18:58:51 -0400

    placed tiles within anchor tags to make the entire tile a clickable link. this caused the tiles to shrink on desktop but maintain previous behaviour on mobile/tablet.

--- src/components/home.js
+++ src/components/home.js
@@ -10,24 +10,25 @@ class Home extends Component {
         <div className="container">
           <div className="tile is-ancestor">
               <div className="tile is-parent">
-                <div className="tile is-child box has-background-primary has-text-centered">
-                  <figure class="image is-96x96 is-inline-block" >
-                    <img src={questionMark} alt="question mark" />
-                  </figure>
-                  <a href="/random">
-                    <p className="title has-text-light has-text-centered">Random Recipe</p>
-                  </a>
-                </div>
+                <a href="/random">
+                  <div className="tile is-child box has-background-primary has-text-centered">
+                    <figure class="image is-96x96 is-inline-block" >
+                      <img src={questionMark} alt="question mark" />
+                    </figure>
+                      <p className="title has-text-light has-text-centered">Random Recipe</p>
+                  </div>
+                </a>
               </div>
               <div className="tile is-parent">
-                <div className="tile is-child box has-background-primary has-text-centered">
-                  <figure class="image is-96x96 is-inline-block" >
-                    <img src={ramen} alt="ramen" />
-                  </figure>
-                  <a href="/random">
+                <a href="/cuisines">
+                  <div className="tile is-child box has-background-primary has-text-centered">
+                    <figure class="image is-96x96 is-inline-block" >
+                      <img src={ramen} alt="ramen" />
+                    </figure>
                     <p className="title has-text-light has-text-centered">Cuisines</p>
-                  </a>
                 </div>
+                </a>
+                
               </div>
           </div>
         </div>
