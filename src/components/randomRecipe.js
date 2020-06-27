0b1ef17ffebd392f117c31dd50a16aeb6a3521fa
Ben Governali <bgovern93@gmail.com>
2020-06-27 13:25:54 -0400

    created working branch, changed getRecipe function on randomRecipe route to ping the new back end and retrieve a random recipe from spoonacular. recipe is now stored in state on componentDidMount

--- src/components/randomRecipe.js
+++ src/components/randomRecipe.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-const API_KEY = process.env.REACT_APP_SPOONACULAR_API_KEY;
-
 class RandomRecipe extends Component {
   state = {
     recipeData: []
@@ -9,17 +7,16 @@ class RandomRecipe extends Component {
 
   async componentDidMount() {
     const recipe = await this.getRecipe();
+    console.log("RECIPE IS: ", recipe);
     this.setState({
-      recipeData: recipe
+      recipeData: recipe.recipes[0]
     })
     console.log(this.state);
   }
 
   getRecipe = async () => {
-    const response = 
-      await fetch("https://api.spoonacular.com/recipes/random", 
-        { headers: {"X-RapidAPI-Key": API_KEY}});
-    console.log(await response.json());
+    const response = await fetch("http://localhost:3000/random");
+    console.log("RESPONSE IS: ", response);
     return response.json();
   }
   
