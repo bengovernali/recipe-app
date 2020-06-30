25bcc778f554dd221c73b9415d12b6bbef9307cb
Ben Governali <bgovern93@gmail.com>
2020-06-30 19:45:55 -0400

    rendering random recipe image, ingredients, and steps

--- src/components/randomRecipe.js
+++ src/components/randomRecipe.js
@@ -2,26 +2,61 @@ import React, { Component } from "react";
 
 class RandomRecipe extends Component {
   state = {
-    recipeData: []
+    recipeData: null
   };
 
   async componentDidMount() {
+    await this.loadData();
+    console.log(this.state.recipeData);
+  }
+
+  async loadData() {
     const recipe = await this.getRecipe();
-    console.log("RECIPE IS: ", recipe);
     this.setState({
       recipeData: recipe.recipes[0]
     })
-    console.log(this.state);
   }
 
   getRecipe = async () => {
     const response = await fetch("http://localhost:3000/random");
-    console.log("RESPONSE IS: ", response);
     return response.json();
   }
   
   render() {
-    return <div>Random Recipe will be generated here</div>;
+    if (!this.state.recipeData) {
+      return <div />
+    } 
+    const recipe = this.state.recipeData;
+    return (
+      <div className="section">
+        <div className="box">
+          <img src={`${recipe.image}`} alt={`${recipe.title}`} />
+          <h2 className="title is-2">{recipe.title}</h2>
+          <h3 className="title is-3">Ingredients</h3>
+          <div className="content">
+              <ul>
+                {recipe.extendedIngredients.map((ingredient, i) => {
+                    return (
+                      <li key={i}>{`${ingredient.measures.us.amount} ${ingredient.measures.us.unitShort} ${ingredient.name}`}</li>
+                    )
+                  })
+                }
+              </ul>  
+          </div>
+          <h3 className="title is-3">Instructions</h3>
+          <div className="content">
+            <ol>
+              {recipe.analyzedInstructions[0].steps.map((instruction, i) => {
+                  return (
+                    <li key={i}>{`${instruction.step}`}</li>
+                  )
+                })
+              }
+            </ol>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
